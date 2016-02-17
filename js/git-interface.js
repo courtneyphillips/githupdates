e452d6b2c1bc035e3c5072bda111538984b06c17
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-02-17 11:47:59 -0800

    fix indentation.

--- js/git-interface.js
+++ js/git-interface.js
@@ -4,11 +4,10 @@ $(document).ready(function(){
     dataType: "json",
     userAgent: "test",
     success: function(returndata){
-      console.log(returndata);
       $('#githubUrl').text("your Github URL is " + returndata.url)
       $('#location').text("You are located in " + returndata.location)
       $('#followers').text("You currently have " + returndata.followers + " followers.")
       $('#json').text(JSON.stringify(returndata, null, 2));
     }
-    })
   });
+});
