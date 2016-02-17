5b81c9e650421add8a9c9266d04667f91ab2cf54
Courtney Phillips <courtney.mae.phillips@gmail.com>
2016-02-17 08:47:40 -0800

    add gulp tasks for concatenation and browserfying.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
 var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+
+gulp.task('concatInterface', function(){
+  return gulp.src(['./js/browser.js', './js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify'j, ['concatInterface'], function(){
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+  .bundle()
+  .pipe(source('app.js'))
+  .pipe(gulp.dest('./build/js'));
+});
 
 gulp.task('jshint', function(){
   return gulp.src(['js/*.js'])
